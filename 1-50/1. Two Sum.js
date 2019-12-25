72f81ada119b0a298a786c9facdfa5a63c7af70e
Lisianthus-A <lisianthus.zeng@gmail.com>
2019-12-25 14:52:58 +0800

    initial

---
+++ 1-50/1. Two Sum.js
@@ -0,0 +1,20 @@
+﻿/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let ret = new Array();
+    for(i = 0; i < nums.length; i++)
+    {
+        for(j = i + 1; j < nums.length; j++)
+        {
+            if(nums[i] + nums[j] == target)
+            {
+                ret[0] = i;
+                ret[1] = j;
+                return ret;
+            }
+        }
+    }
+};
