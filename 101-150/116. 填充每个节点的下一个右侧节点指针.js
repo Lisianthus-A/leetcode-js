ae17c70d78939018692c44b836100a31674ebbc7
Lisianthus-A <lisianthus.zeng@gmail.com>
2020-06-04 14:13:37 +0800

    116

---
+++ 101-150/116. 填充每个节点的下一个右侧节点指针.js
@@ -0,0 +1,47 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) {
+
+    //逐层遍历节点
+    const levelTraverse = (node) => {
+        if (node === null) {
+            return [];
+        }
+        let result = [[node]];
+        let queue = [node];  //当前层树节点
+        while (queue[0]) {
+            let nextQueue = [];  //下一层的树节点
+            for (let node of queue) {
+                node.left ? nextQueue.push(node.left) : null;
+                node.right ? nextQueue.push(node.right) :null;
+            }
+            if (nextQueue.length > 0) {
+                result.push(nextQueue);
+            }
+            queue = nextQueue;
+        }
+        return result;
+    }
+
+    let levelNodes = levelTraverse(root);
+
+    for (let nodeArr of levelNodes) {
+        for (let i = 0; i < nodeArr.length; i++) {
+            nodeArr[i].next = nodeArr[i + 1] || null;
+        }
+    }
+
+    return root;
+};
