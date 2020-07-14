4ad9b34f4f9bbf21e00c2f2d88a76445058ba0b7
Lisianthus-A <lisianthus.zeng@gmail.com>
2020-07-14 19:09:41 +0800

    206

---
+++ 201-250/206. 反转链表.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if (!head) {
+        return null;
+    }
+    
+    let nodes = [];
+    while (head) {
+        nodes.push(head);
+        head = head.next;
+    }
+    nodes.reverse();
+
+    for (let i = 0; i < nodes.length - 1; i++) {
+        nodes[i].next = nodes[i + 1];
+    }
+    nodes[nodes.length - 1].next = null;
+
+    return nodes[0];
+};
