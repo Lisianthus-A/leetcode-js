65ce8bd53eecc35b5a6aa2cda574e6d3810f6c93
Lisianthus-A <lisianthus.zeng@gmail.com>
2020-08-27 23:31:25 +0800

    342

---
+++ answers/342. 4的幂.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPowerOfFour = function(num) {
+    if (num === 0) {
+        return false;
+    }
+    
+    while (num % 4 === 0) {
+        num /= 4;
+    }
+    return num === 1;
+};
