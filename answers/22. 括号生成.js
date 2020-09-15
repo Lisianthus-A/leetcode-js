83be9a42ec0d7030c3faffa6bbe27be2475d22eb
Lisianthus-A <lisianthus.zeng@gmail.com>
2020-09-15 22:05:36 +0800

    22

--- answers/22. 括号生成.js
+++ answers/22. 括号生成.js
@@ -3,66 +3,23 @@
  * @return {string[]}
  */
 var generateParenthesis = function (n) {
-    let fullArr = ['0'];
-    //生成全排列，由fullArr保存
-    for (let i = 1; i < n * 2; i++) {
-        let len = fullArr.length;
-        for (let j = 0; j < len; j++) {
-            for (let k in fullArr[j]) {
-                let arr = fullArr[j].split('');
-                arr.splice(k, 0, i.toString());
-                fullArr.push(arr.join(''));
-            }
-            fullArr.push(fullArr[j] + i.toString());
+    const helper = (left, right, str) => {
+        if (left === n && left === right) {
+            res.push(str);
+            return;
         }
-        fullArr = fullArr.slice(len);
-    }
-    //根据fullArr生成括号数组
-    let res = [];
-    for (let i of fullArr) {
-        let str = '';
-        for (let j of i) {
-            if (j % 2 === 0) {
-                str += '(';
-            } else {
-                str +=')';
-            }
+        //左括号未用完，添加左括号
+        if (left < n) {
+            helper(left + 1, right, str + '(');
         }
-        res.push(str);
-    }
-    res = res.filter(isValid);
-    let arr = [];
-    for (let i of res) {  //去重
-        if (arr.indexOf(i) === -1) {
-            arr.push(i);
+        //添加右括号后可闭合，并且右括号未用完
+        if (left > right && right < n) {
+            helper(left, right + 1, str + ')');
         }
     }
-    res = arr;
+
+    const res = [];
+
+    helper(0, 0, '');
     return res;
-};
-var isValid = function(s) {
-    let relation = {  //对应关系
-        ')': '(',
-        '}': '{',
-        ']': '['
-    };
-    let stack = [];
-    //遇到左括号让其入栈，遇到右括号与栈内元素匹配
-    for (let i of s) {
-        switch(i) {
-            case '(':
-            case '{':
-            case '[':
-                stack.push(i);
-                break;
-            default:
-                if (relation[i] !== stack.pop()) {
-                    return false;
-                }
-        }
-    }
-    if (stack.length !== 0) {  //栈内还有左括号未匹配
-        return false;
-    }
-    return true;
 };
