510aca676cca41e72ccc9e006f33bdfab3fa7719
lisianthus-a <1419033252@qq.com>
2020-12-30 10:06:35 +0800

    553

---
+++ answers/501-1000/0553. 最优除法.ts
@@ -0,0 +1,7 @@
+function optimalDivision(nums: number[]): string {
+    if (nums.length <= 2) {
+        return nums.join('/');
+    }
+    
+    return `${nums[0]}/(${nums.slice(1).join('/')})`;
+};
