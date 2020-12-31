9929c3c5c9605b5cb6b22d00a52a8babaf2078b4
lisianthus-a <1419033252@qq.com>
2020-12-31 15:25:12 +0800

    559

---
+++ answers/501-1000/0559. N 叉树的最大深度.js
@@ -0,0 +1,15 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+    return root ? Math.max(...root.children.map(node => maxDepth(node) + 1), 1) : 0;
+};
